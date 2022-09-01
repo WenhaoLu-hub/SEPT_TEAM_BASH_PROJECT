5c6ccd392bbdc56943dba80dee8bd270255ecc1a
Sifan-Gao <s3753605@student.rmit.edu.au>
2022-09-01 22:51:04 +0800

    user model

---
+++ springboot_backend/src/main/java/com/example/springbootbackend/model/User.java
@@ -0,0 +1,58 @@
+package com.example.springbootbackend.model;
+
+public class User {
+    String firstName;
+    String lastName;
+    String gender;
+    int age;
+    int phoneNumber;
+    String address;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
