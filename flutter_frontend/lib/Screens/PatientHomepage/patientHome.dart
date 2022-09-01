bd091aaf6480135f50ee96b0e703cfa3e656cc81
Alphonso12345 <s3843450@student.rmit.edu.au>
2022-09-01 18:35:50 +1000

    start

---
+++ flutter_frontend/lib/Screens/PatientHomepage/patientHome.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:team_bash_project/Screens/login/login.dart';
+import 'package:team_bash_project/Screens/Welcome/components/roundedButton.dart';
+
+class PatientHome extends StatelessWidget {}
+    const PatientHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size deviceSize = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[]
+      )
+    );
+  }
