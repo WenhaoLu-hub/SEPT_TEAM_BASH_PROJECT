c9f82146b6a008abc6a41cef4e35c013f0f1e27b
Sifan-Gao <s3753605@student.rmit.edu.au>
2022-09-02 00:02:02 +0800

    user dao

---
+++ springboot_backend/src/main/java/com/example/springbootbackend/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.example.springbootbackend.dao;
+
+import com.example.springbootbackend.model.User;
+import org.springframework.stereotype.Service;
+
+public interface UserDao {
+    void add(User user);
+    void delete(int id);
+    void change(User user);
+    String search(User user);
+
+}
