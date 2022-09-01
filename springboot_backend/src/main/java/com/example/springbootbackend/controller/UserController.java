005e858d1dfb6996e93b1730c8c6dff5e91c803d
Sifan-Gao <s3753605@student.rmit.edu.au>
2022-09-02 00:02:11 +0800

    user controller

---
+++ springboot_backend/src/main/java/com/example/springbootbackend/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.example.springbootbackend.controller;
+
+import com.example.springbootbackend.model.User;
+import com.example.springbootbackend.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @PostMapping("/add")
+    public void add(@RequestBody User user){
+        userService.add(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        userService.delete(id);
+    }
+
+    @PutMapping("/change")
+    public void change(@RequestBody User user){
+        userService.change(user);
+    }
+
+    @GetMapping("")
+    public String search(@RequestBody User user){
+        return userService.search(user);
+    }
+
+}
