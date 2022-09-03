cd1c41bb2056866707db2cd309d443ec8105283c
Sifan-Gao <s3753605@student.rmit.edu.au>
2022-09-03 15:21:09 +0800

    add Mapper scanner by manual

--- springboot_backend/src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
+++ springboot_backend/src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
@@ -1,9 +1,11 @@
 package com.example.springbootbackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.springbootbackend.mapper")
 public class SpringbootBackendApplication {
 
 	public static void main(String[] args) {
