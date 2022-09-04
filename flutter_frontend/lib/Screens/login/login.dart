8936d4a5c4bd606ae8de0c49c66dc4569a93ff38
s3810097_WenhaoLu <s3810097@student.rmit.edu.au>
2022-09-05 02:16:45 +1000

    add logo and title on signup page

--- flutter_frontend/lib/Screens/login/login.dart
+++ flutter_frontend/lib/Screens/login/login.dart
@@ -9,7 +9,6 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-
     return Scaffold(
         body: SizedBox(
       width: double.infinity,
--- flutter_frontend/lib/Screens/signup/signup.dart
+++ flutter_frontend/lib/Screens/signup/signup.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 70,
+          ),
+          _logo(context),
+          const SizedBox(
+            height: 40,
+          ),
+          _loginLabel(),
+        ],
+      ),
+    );
   }
+}
+
+Widget _logo(BuildContext context) {
+  Size size = MediaQuery.of(context).size;
+  return Center(
+    child: SizedBox(
+      child: Image(
+          width: size.width * 0.2,
+          image: const AssetImage("assets/images/logo.png")),
+    ),
+  );
+}
+
+Widget _loginLabel() {
+  return Center(
+    child: Text(
+      "Sign Up",
+      style: GoogleFonts.josefinSans(
+          textStyle: const TextStyle(
+        color: Color(0xff164276),
+        fontWeight: FontWeight.w900,
+        fontSize: 40,
+      )),
+    ),
+  );
 }
