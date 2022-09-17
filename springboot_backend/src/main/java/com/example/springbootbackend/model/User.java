980d6202c9c7c5cd399c9d0a69f4e3249be12166
Dised <103350521+Dised@users.noreply.github.com>
2022-09-17 14:52:13 +1000

    Update User model for register

--- springboot_backend/src/main/java/com/example/springbootbackend/model/User.java
+++ springboot_backend/src/main/java/com/example/springbootbackend/model/User.java
@@ -1,34 +1,6 @@
 package com.example.springbootbackend.model;
 
 public class User {
-<<<<<<< HEAD
-    String UserName;
-    String password;
-    int id;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return UserName;
-    }
-
-    public void setUserName(String firstName) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-=======
     String firstName;
     String lastName;
     String gender;
@@ -36,6 +8,10 @@ public class User {
     int phoneNumber;
     String address;
     Long id;
+
+    String email;
+    String password;
+    String type;
 
     public Long getId() {
         return id;
@@ -91,6 +67,30 @@ public class User {
 
     public void setAddress(String address) {
         this.address = address;
->>>>>>> origin/login
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
 }
