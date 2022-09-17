040fc22ad34e9fcfe9df5a2a1eecf03f8ab91c63
Alphonso12345 <s3843450@student.rmit.edu.au>
2022-09-17 20:23:24 +1000

    ui complete

--- flutter_frontend/lib/Screens/PatientProfilePage/patientProfile.dart
+++ flutter_frontend/lib/Screens/PatientProfilePage/patientProfile.dart
@@ -3,6 +3,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter/cupertino.dart';
+import 'package:team_bash_project/Screens/PatientProfilePage/patientProfileForm.dart';
+import 'package:team_bash_project/Widgets/BlackBorderButton.dart';
 
 class PatientProfile extends StatefulWidget {
   const PatientProfile({Key? key}) : super(key: key);
@@ -14,30 +16,37 @@ class PatientProfile extends StatefulWidget {
 class _PatientProfileState extends State<PatientProfile> {
   @override
   Widget build(BuildContext context) {
-    Size deviceSize = MediaQuery.of(context).size;
+    // Size deviceSize = MediaQuery.of(context).size;
     return Scaffold(
         body: Container(
       padding: const EdgeInsets.all(16),
-      width: deviceSize.width,
+      width: double.infinity,
       color: Colors.white,
-      child: Row(mainAxisAlignment: MainAxisAlignment.center,
-          // ignore: prefer_const_literals_to_create_immutables
-          children: [
-            const SizedBox(
-              height: 32,
-            ),
-            const Text(
-              'Patient Profile',
-              style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.w400,
-                  fontFamily: 'OpenSans'),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 60,
-            ),
-          ]),
+      child: SingleChildScrollView(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              const SizedBox(
+                height: 32,
+              ),
+              const Text(
+                'Patient Profile',
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w400,
+                    fontFamily: 'OpenSans'),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const patientProfileFormPage(),
+              const SizedBox(
+                height: 30,
+              ),
+              BlackBorderButton(buttonText: "Log out", onPressed: null)
+            ]),
+      ),
     ));
   }
 }
--- flutter_frontend/lib/main.dart
+++ flutter_frontend/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_state_manager/get_state_manager.dart';
 import 'package:team_bash_project/Screens/PatientHealthConditionPage/patientHealthCondition.dart';
 import 'package:team_bash_project/Screens/PatientProfilePage/patientProfile.dart';
+import 'package:team_bash_project/Screens/PatientProfilePage/patientProfileForm.dart';
 import 'package:team_bash_project/Screens/Welcome/welcome.dart';
 
 import 'Screens/PatientHomepage/patientHome.dart';
@@ -15,7 +18,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SEPT_Project',
       theme: ThemeData(),
