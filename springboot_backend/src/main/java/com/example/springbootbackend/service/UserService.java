5be2a1ef3081245ea3dcf5a86b5e9443fc757dea
Dised <103350521+Dised@users.noreply.github.com>
2022-09-17 15:07:51 +1000

    delete unused controller and service

--- springboot_backend/src/main/java/com/example/springbootbackend/service/UserService.java
+++ springboot_backend/src/main/java/com/example/springbootbackend/service/UserService.java
@@ -5,6 +5,7 @@ import com.example.springbootbackend.model.User;
 public interface UserService {
 
     public String search(User user);
+
     public void add(User user);
     public void delete(Long id);
     public void change(User user);
