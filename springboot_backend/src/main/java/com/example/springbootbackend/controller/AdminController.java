3277ccb81ac568e4d2d8f6e4df9eacdcc285429f
KaidaZhangS3823450 <s3823450@student.rmit.edu.au>
2022-10-06 01:28:01 +1100

    write basic function

--- springboot_backend/src/main/java/com/example/springbootbackend/controller/AdminController.java
+++ springboot_backend/src/main/java/com/example/springbootbackend/controller/AdminController.java
@@ -1,10 +1,50 @@
 package com.example.springbootbackend.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.springbootbackend.model.User;
+import com.example.springbootbackend.service.AdminService;
+import com.example.springbootbackend.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+
+    @Resource
+    private AdminService adminService;
+
+    @PostMapping("/add")
+    public void add(@RequestBody User user){
+        adminService.add(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        adminService.delete(id);
+    }
+
+    @GetMapping("/doctors")
+    public Object getDoctors(){
+        List<User> doctors = adminService.getDoctors("doctor");
+        Map<String, Object> back = new HashMap<String, Object>();
+        back.put("code", 1);
+        back.put("msg", "doctor list");
+        back.put("doctors",doctors);
+        return back;
+    }
+    @GetMapping("/patients")
+    public Object getPatients(){
+        List<User> doctors = adminService.getPatients("patient");
+        Map<String, Object> back = new HashMap<String, Object>();
+        back.put("code", 1);
+        back.put("msg", "patient list");
+        back.put("patient",doctors);
+        return back;
+    }
+
 }
