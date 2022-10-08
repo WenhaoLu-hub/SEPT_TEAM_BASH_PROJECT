a8305ef93aa9e2e24a2d6628dcdc48ce40d34c83
Alphonso12345 <s3843450@student.rmit.edu.au>
2022-10-08 23:22:34 +1100

    created admin homepage

--- flutter_frontend/lib/main.dart
+++ flutter_frontend/lib/main.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:team_bash_project/Screens/PatientHealthConditionPage/patientHealthCondition.dart';
 import 'package:team_bash_project/Screens/PatientProfilePage/patientProfile.dart';
+import 'package:team_bash_project/Screens/admin/adminHomepage.dart';
 import 'package:team_bash_project/Screens/booking/booking.dart';
 import 'package:team_bash_project/Screens/landing_page/landing_page.dart';
 import 'package:team_bash_project/Screens/login/login.dart';
 import 'package:team_bash_project/Screens/patientHomepage/patientHome.dart';
 import 'package:team_bash_project/Screens/signup/signup.dart';
 import 'package:team_bash_project/Screens/welcome/welcome.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -23,17 +23,20 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SEPT_Project',
-      theme: ThemeData(platform:TargetPlatform.iOS,),
+      theme: ThemeData(
+        platform: TargetPlatform.iOS,
+      ),
       getPages: [
-        GetPage(name:"/home",page:()=>const Welcome()),
-        GetPage(name:"/login",page:()=>const Login()),
-        GetPage(name:"/signup",page:()=>const SignUp()),
-        GetPage(name:"/landingpage",page:()=>const LandingPage()),
-        GetPage(name:"/patient",page:()=> const PatientHome()),
-        GetPage(name:"/patient/booking",page:()=> const Booking()),
-        GetPage(name:"/patient/profile",page:()=> const PatientProfile()),
-        GetPage(name:"/patient/condition",page:()=> const PatientHealthCondition()),
-        
+        GetPage(name: "/home", page: () => const AdminHomepage()),
+        GetPage(name: "/login", page: () => const Login()),
+        GetPage(name: "/signup", page: () => const SignUp()),
+        GetPage(name: "/landingpage", page: () => const LandingPage()),
+        GetPage(name: "/patient", page: () => const PatientHome()),
+        GetPage(name: "/patient/booking", page: () => const Booking()),
+        GetPage(name: "/patient/profile", page: () => const PatientProfile()),
+        GetPage(
+            name: "/patient/condition",
+            page: () => const PatientHealthCondition()),
       ],
       initialRoute: '/home',
     );
